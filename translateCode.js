529a84778b73571fb5da629f99fdb2bb9f81a7a3
Travis Genzer <tlgenzer@gmail.com>
2023-03-25 19:15:36 -0500

    Functions

--- translateCode.js
+++ translateCode.js
@@ -1,14 +1,17 @@
-// Define a list of sentences and their corresponding languages
-const sentences = [
-    { sentence: "Hello, how are you?", language: "English" },
-    { sentence: "Bonjour, comment ça va?", language: "French" },
-    { sentence: "Hola, cómo estás?", language: "Spanish" },
-    { sentence: "你好，你好吗？", language: "Chinese" },
-    { sentence: "こんにちは、お元気ですか？", language: "Japanese" },
-];
 
-function winCheck()
+function createSentences(){
+    // Define a list of sentences and their corresponding languages
+    const sentences = [
+        { sentence: "Hello, how are you?", language: "English" },
+        { sentence: "Bonjour, comment ça va?", language: "French" },
+        { sentence: "Hola, cómo estás?", language: "Spanish" },
+        { sentence: "你好，你好吗？", language: "Chinese" },
+        { sentence: "こんにちは、お元気ですか？", language: "Japanese" },
+    ];
+}
+function checkSentence()
 {
+    // Check if the user's answer is correct and provide feedback
     if (userLanguage.toLowerCase() === correctLanguage.toLowerCase()) {
         alert("Correct!");
     
@@ -17,17 +20,22 @@ function winCheck()
     }
 
 }
+function sentenceSelect(){
+    // Select a random sentence from the list
+    const randomIndex = Math.floor(Math.random() * sentences.length);
+    const randomSentence = sentences[randomIndex].sentence;
+    const correctLanguage = sentences[randomIndex].language;
+}
 
-// Select a random sentence from the list
-const randomIndex = Math.floor(Math.random() * sentences.length);
-const randomSentence = sentences[randomIndex].sentence;
-const correctLanguage = sentences[randomIndex].language;
+
+function sentencePrompt(){
+    // Prompt the user to input the corresponding language
+    const userLanguage = prompt(`What language is this sentence written in? \n\n"${randomSentence}"`);
+
+}
 
 
-// Prompt the user to input the corresponding language
-const userLanguage = prompt(`What language is this sentence written in? \n\n"${randomSentence}"`);
 
-// Check if the user's answer is correct and provide feedback
 
 
 /*var ua = navigator.userAgent.toLowerCase();
